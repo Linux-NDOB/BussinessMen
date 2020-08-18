ce7861462a81f25cd5dc9ed547af701b79b50001
Ndob <lordndob@gmail.com>
2020-08-18 08:29:21 -0500

    ...

--- Programming II/project/src/main/java/com/company/project/Sudo.java
+++ Programming II/project/src/main/java/com/company/project/Sudo.java
@@ -1,7 +1,7 @@
 package com.company.project;
 
+import javax.swing.Icon;
 import javax.swing.JOptionPane;
-
 
 public class Sudo{
 
@@ -16,9 +16,6 @@ public class Sudo{
 
    
         int i, pos, id;
-        String nomP;
-        float costo;
-        
         String name;
         String gender;
         float basicSalary;
@@ -115,7 +112,70 @@ public class Sudo{
         return -1;
     }
     
+    public void pointTwo(){
+
+        /* 2.Dado un ID Empleado el programa buscara su información en el vector
+         y si existe mostrar sus datos y el valor neto a pagar de ese empleado.	
+         
+         */
+
+    JOptionPane.showMessageDialog(null,"Now you will find a employee by id,"+"\n"
+    +"show his data and the net value yo pay" );
+
+     int i; int id;
+     id =Integer.parseInt(JOptionPane.showInputDialog(null, "Enter the id to search"));
+     
+       for (i = 0; i < gnu.length; i ++) {
+          
+            if (gnu[i].getId() == id) {
+     
+     
+     JOptionPane.showMessageDialog(null, "The entered id is: "+ gnu[i].getId() + "\n" +
+                                          "Basic salary : " + gnu[i].getBasicSalary() + "\n" +
+                                          "Overtime Worked : " + gnu[i].getOvtermineWorked() + "\n" +
+                                          ""
+                                          ); 
+    
+     
+            }return;
+            
+       }
+ 
+ JOptionPane.showMessageDialog(null,"Codigo no registrado");
+}
+        
+
+        
+
+
+
+
+
+
+        /*
+        El neto a pagar de cada empleado es calculado bajo las siguientes condiciones:
+
+Se debe descontar 4% del salario básico para aportes a salud, 3,75% para aportes a pensión y 2% para ARL.
+Se debe sumar en el neto a pagar el devengado por horas extra que se obtiene de la cantidad laborada por el valor de las H.E
+La hora extra tendrá un valor de $45.000 si el empleado tiene más de 10 años en la empresa, de $35.000 si tiene entre 10 y 5 años, de $ 30.000 si tiene entre 5 y 3 años y de $ 25.000 si tiene menos de 3 años.
+Los empleados de ESTRATO 1 y 2 tienen un subsidio de transporte equivalente a $78.000.
+
+Al finalizar se debe arrojar el detallado de nómina para el empleado donde especifica sus ganancias, descuentos y el neto a pagar así:
+
+
+SUELDO BASICO			xxxxx
+H.E LABORADAS			xxxxx	
+VALOR H.E				xxxxx
+TOTAL H.E				xxxxx
+APORTE SALUD			xxxxx
+APORTE PENSION			xxxxx
+APORTE ARL				xxxxx	
+SUBSIDIO TTE				xxxxx
+*/
+
+
+
+    }
 
 
     
-}
